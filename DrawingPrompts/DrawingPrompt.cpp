d521e8b28019ce6953e1e194af3d367b787f686c
SeigiMitsuko <60911422+SeigiMitsuko@users.noreply.github.com>
2023-03-15 11:24:17 -0500

    Adding another type of drawing prompt after some time away from this program.

--- DrawingPrompts/DrawingPrompt.cpp
+++ DrawingPrompts/DrawingPrompt.cpp
@@ -4,7 +4,11 @@
     // make words come from a txt file
         // completed May 10
     // Just remembered to make an option to repeat.  Start at the random number generator
-        // completed May 15
+        // completed May 15, 2022
+    // After some time away, I've decided to make an option to decide what kind of prompt
+    //you want.
+        // first on the list, get two animals and an option to draw one or a combo
+            // completed March 3, 2023
 
 #include <iostream>
 #include <stdio.h>
@@ -89,6 +93,23 @@ int main()
     }
     fileC.close();
 
+    // open animals file
+    ifstream fileD("Animals.txt");
+    if(!fileD)
+    {
+        cout << "Cannot open file";
+        return 0;
+    }
+        // read all words in file into an array
+    tempInt = 0;
+    string myAnimals[734];
+    while(getline (fileD, tempString))
+    {
+        myAnimals[tempInt] = tempString;
+        tempInt++;
+    }
+    fileD.close();
+
     // first draft code commented out below
 
     //string myAdjetives[25] = {"thirsty", "selfish", "defective", "exuberant", "bent",
@@ -108,17 +129,61 @@ int main()
 
     while(repeatProgram == true)
     {
-            // assign random numbers to be used for each set of words
-        random1 = (rand() % 25) + 1;
-        random2 = (rand() % 25) + 1;
-        random3 = (rand() % 25) + 1;
+        cout << "Do you want to draw an animal (A) or get a three-word prompt(B)?" << endl;
+        cin >> answer;
+        
+            // display the prompt
+        if(answer == "A" || answer == "a")
+        {
+                // set random numbers for the animal(s) chosen and whether to draw one
+                // or a combo
+            random1 = (rand() % 734) + 1;
+            random2 = (rand() % 734) + 1;
+            random3 = (rand() % 2) + 1; // if 1, it's a single animal. 2 is a combo
 
-            // output words for the prompt
-        cout << "The writing prompt for today is: " << myAdjetives[random1] << ", " << 
-                                                    myNouns[random2] << ", " << 
-                                                    myVerbs[random3] << endl;
+                // just in case the two animals are the same
+            if(random1 == random2)
+            {
+                random3 = 1;
+            }
+                // just in case random3 turns out to be 0. 
+                // probably won't happen, but better safe than sorry.
+            if(random3 == 0)
+            {
+                random3 = 1;
+            }
+            
+            switch(random3)
+            {
+                case 1:
+                    cout << "The animal you are to draw is: " << myAnimals[random1] 
+                    << endl;
+                    break;
+                case 2:
+                    cout << "The animal you are to draw is a combination between " 
+                        << myAnimals[random1] << " and " << myAnimals[random2] << endl;
+                    break;
+            }
 
-        cout << endl << "Do you want to get a new prompt? Y/N:";
+        } 
+        else if(answer == "B" || answer == "b")
+        {
+                // assign random numbers to be used for each set of words
+            random1 = (rand() % 25) + 1;
+            random2 = (rand() % 25) + 1;
+            random3 = (rand() % 25) + 1;
+
+                // output words for the prompt
+            cout << "The writing prompt for today is: " << myAdjetives[random1] << 
+                    ", " << myNouns[random2] << ", " << myVerbs[random3] << endl;
+        }
+        else
+        {
+            cout << "You didn't pick a choice." << "\n";
+        }
+        
+
+        cout << endl << "Do you want to get a new prompt? Y/N:" << endl;
         cin >> answer;
 
         answerChar = answer.at(0);
